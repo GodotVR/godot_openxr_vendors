d68a254cd352e6b4b19e589e6e40a5414182015f
Logan Lang <devloglogan@gmail.com>
2024-04-30 14:20:55 -0500

    Implement XR_FB_composition_layer_settings extension wrapper

---
+++ common/src/main/cpp/extensions/openxr_fb_composition_layer_settings_extension_wrapper.cpp
@@ -0,0 +1,164 @@
+/**************************************************************************/
+/*  openxr_fb_composition_layer_settings_extension_wrapper.cpp            */
+/**************************************************************************/
+/*                       This file is part of:                            */
+/*                              GODOT XR                                  */
+/*                      https://godotengine.org                           */
+/**************************************************************************/
+/* Copyright (c) 2022-present Godot XR contributors (see CONTRIBUTORS.md) */
+/*                                                                        */
+/* Permission is hereby granted, free of charge, to any person obtaining  */
+/* a copy of this software and associated documentation files (the        */
+/* "Software"), to deal in the Software without restriction, including    */
+/* without limitation the rights to use, copy, modify, merge, publish,    */
+/* distribute, sublicense, and/or sell copies of the Software, and to     */
+/* permit persons to whom the Software is furnished to do so, subject to  */
+/* the following conditions:                                              */
+/*                                                                        */
+/* The above copyright notice and this permission notice shall be         */
+/* included in all copies or substantial portions of the Software.        */
+/*                                                                        */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,        */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF     */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. */
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY   */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,   */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE      */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */
+/**************************************************************************/
+
+#include "extensions/openxr_fb_composition_layer_settings_extension_wrapper.h"
+
+#include <godot_cpp/classes/open_xrapi_extension.hpp>
+#include <godot_cpp/variant/utility_functions.hpp>
+
+using namespace godot;
+
+static const char *SUPERSAMPLING_MODE_PROPERTY_NAME = "XR_FB_composition_layer_settings/supersampling_mode";
+static const char *SHARPENING_MODE_PROPERTY_NAME = "XR_FB_composition_layer_settings/sharpening_mode";
+
+OpenXRFbCompositionLayerSettingsExtensionWrapper *OpenXRFbCompositionLayerSettingsExtensionWrapper::singleton = nullptr;
+
+OpenXRFbCompositionLayerSettingsExtensionWrapper *OpenXRFbCompositionLayerSettingsExtensionWrapper::get_singleton() {
+	if (singleton == nullptr) {
+		singleton = memnew(OpenXRFbCompositionLayerSettingsExtensionWrapper());
+	}
+	return singleton;
+}
+
+OpenXRFbCompositionLayerSettingsExtensionWrapper::OpenXRFbCompositionLayerSettingsExtensionWrapper() :
+		OpenXRExtensionWrapperExtension() {
+	ERR_FAIL_COND_MSG(singleton != nullptr, "An OpenXRFbCompositionLayerSettingsExtensionWrapper singleton already exists.");
+
+	request_extensions[XR_FB_COMPOSITION_LAYER_SETTINGS_EXTENSION_NAME] = &fb_composition_layer_settings;
+	singleton = this;
+}
+
+OpenXRFbCompositionLayerSettingsExtensionWrapper::~OpenXRFbCompositionLayerSettingsExtensionWrapper() {
+	cleanup();
+}
+
+void OpenXRFbCompositionLayerSettingsExtensionWrapper::_bind_methods() {
+	BIND_ENUM_CONSTANT(SUPERSAMPLING_MODE_DISABLED);
+	BIND_ENUM_CONSTANT(SUPERSAMPLING_MODE_NORMAL);
+	BIND_ENUM_CONSTANT(SUPERSAMPLING_MODE_QUALITY);
+
+	BIND_ENUM_CONSTANT(SHARPENING_MODE_DISABLED);
+	BIND_ENUM_CONSTANT(SHARPENING_MODE_NORMAL);
+	BIND_ENUM_CONSTANT(SHARPENING_MODE_QUALITY);
+}
+
+void OpenXRFbCompositionLayerSettingsExtensionWrapper::cleanup() {
+	fb_composition_layer_settings = false;
+}
+
+Dictionary OpenXRFbCompositionLayerSettingsExtensionWrapper::_get_requested_extensions() {
+	Dictionary result;
+	for (auto ext : request_extensions) {
+		uint64_t value = reinterpret_cast<uint64_t>(ext.value);
+		result[ext.key] = (Variant)value;
+	}
+	return result;
+}
+
+uint64_t OpenXRFbCompositionLayerSettingsExtensionWrapper::_set_viewport_composition_layer_and_get_next_pointer(const void *p_layer, const Dictionary &p_property_values, void *p_next_pointer) {
+	if (!fb_composition_layer_settings) {
+		return reinterpret_cast<uint64_t>(p_next_pointer);
+	}
+
+	const XrCompositionLayerBaseHeader *layer = reinterpret_cast<const XrCompositionLayerBaseHeader *>(p_layer);
+
+	if (!layer_structs.has(layer)) {
+		layer_structs[layer] = {
+			XR_TYPE_COMPOSITION_LAYER_SETTINGS_FB, // type
+			p_next_pointer, // next
+			0, // layerFlags
+		};
+	}
+
+	XrCompositionLayerSettingsFB *settings = layer_structs.getptr(layer);
+
+	settings->layerFlags = 0;
+
+	switch ((SupersamplingMode)(int)p_property_values.get(SUPERSAMPLING_MODE_PROPERTY_NAME, SUPERSAMPLING_MODE_DISABLED)) {
+		case SUPERSAMPLING_MODE_NORMAL: {
+			settings->layerFlags |= XR_COMPOSITION_LAYER_SETTINGS_NORMAL_SUPER_SAMPLING_BIT_FB;
+		} break;
+		case SUPERSAMPLING_MODE_QUALITY: {
+			settings->layerFlags |= XR_COMPOSITION_LAYER_SETTINGS_QUALITY_SUPER_SAMPLING_BIT_FB;
+		} break;
+	}
+
+	switch ((SharpeningMode)(int)p_property_values.get(SHARPENING_MODE_PROPERTY_NAME, SHARPENING_MODE_DISABLED)) {
+		case SHARPENING_MODE_NORMAL: {
+			settings->layerFlags |= XR_COMPOSITION_LAYER_SETTINGS_NORMAL_SHARPENING_BIT_FB;
+		} break;
+		case SHARPENING_MODE_QUALITY: {
+			settings->layerFlags |= XR_COMPOSITION_LAYER_SETTINGS_QUALITY_SHARPENING_BIT_FB;
+		} break;
+	}
+
+	if (settings->layerFlags == 0) {
+		return reinterpret_cast<uint64_t>(p_next_pointer);
+	}
+
+	return reinterpret_cast<uint64_t>(settings);
+}
+
+void OpenXRFbCompositionLayerSettingsExtensionWrapper::_on_viewport_composition_layer_destroyed(const void *p_layer) {
+	if (fb_composition_layer_settings) {
+		const XrCompositionLayerBaseHeader *layer = reinterpret_cast<const XrCompositionLayerBaseHeader *>(p_layer);
+		layer_structs.erase(layer);
+	}
+}
+
+TypedArray<Dictionary> OpenXRFbCompositionLayerSettingsExtensionWrapper::_get_viewport_composition_layer_extension_properties() {
+	TypedArray<Dictionary> properties;
+
+	{
+		Dictionary supersampling_mode;
+		supersampling_mode["name"] = SUPERSAMPLING_MODE_PROPERTY_NAME;
+		supersampling_mode["type"] = Variant::INT;
+		supersampling_mode["hint"] = PROPERTY_HINT_ENUM;
+		supersampling_mode["hint_string"] = "Disabled,Normal,Quality";
+		properties.push_back(supersampling_mode);
+	}
+
+	{
+		Dictionary sharpening_mode;
+		sharpening_mode["name"] = SHARPENING_MODE_PROPERTY_NAME;
+		sharpening_mode["type"] = Variant::INT;
+		sharpening_mode["hint"] = PROPERTY_HINT_ENUM;
+		sharpening_mode["hint_string"] = "Disabled,Normal,Quality";
+		properties.push_back(sharpening_mode);
+	}
+
+	return properties;
+}
+
+Dictionary OpenXRFbCompositionLayerSettingsExtensionWrapper::_get_viewport_composition_layer_extension_property_defaults() {
+	Dictionary defaults;
+	defaults[SUPERSAMPLING_MODE_PROPERTY_NAME] = (int)SUPERSAMPLING_MODE_DISABLED;
+	defaults[SHARPENING_MODE_PROPERTY_NAME] = (int)SHARPENING_MODE_DISABLED;
+	return defaults;
+}
