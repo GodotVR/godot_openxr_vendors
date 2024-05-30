aea1445645df8775f08add7b76e5256a30d5e0f8
Logan Lang <95497005+devloglogan@users.noreply.github.com>
2024-05-30 09:50:30 -0500

    Add fb-composition-layer-sample project (#161)

---
+++ samples/fb-composition-layers-sample/example_panel.gd
@@ -0,0 +1,19 @@
+extends Control
+
+@export var display_string: String = ""
+
+@onready var pointer_left: ColorRect = $PointerLeft
+@onready var pointer_right: ColorRect = $PointerRight
+
+var start_pos: Vector2
+
+func _ready() -> void:
+	start_pos = $Label.position
+	$Label.text = display_string
+
+func _process(delta: float) -> void:
+	$Label.position = Vector2(start_pos.x + (100.0 * sin(Time.get_ticks_msec() * 0.001 * 2)), start_pos.y)
+
+func update_pointers(p_position_left: Vector2, p_position_right: Vector2) -> void:
+	pointer_left.position = size * p_position_left - (0.5 * pointer_left.size)
+	pointer_right.position = size * p_position_right - (0.5 * pointer_left.size)
