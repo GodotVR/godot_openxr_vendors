831119ffb90251dd15c148bf80fdfd0f9cba6f4c
David Snopek <dsnopek@gmail.com>
2024-06-19 15:11:39 -0500

    Record XrSim tests for most of the sample projects

--- samples/meta-passthrough-sample/geometry.gd
+++ samples/meta-passthrough-sample/geometry.gd
@@ -2,4 +2,7 @@ extends OpenXRFbPassthroughGeometry
 
 
 func _process(delta: float) -> void:
-	position.x = 3 * sin(Time.get_ticks_msec() / 1000.0)
+	if not "--xrsim-automated-tests" in OS.get_cmdline_user_args():
+		# When we're running tests via the XR Simulator, we don't want this
+		# to be animated, which can lead to differences in the screenshots.
+		position.x = 3 * sin(Time.get_ticks_msec() / 1000.0)
