702e6bce1686cfa78ba4e4b85e43fe8475fbdbdc
devloglogan <devloglogan@gmail.com>
2024-10-14 11:01:42 -0500

    Implement XR_FB_space_warp extension

---
+++ samples/meta-space-warp-sample/multi_mesh_move.gd
@@ -0,0 +1,13 @@
+extends MultiMeshInstance3D
+
+@export var direction: Vector3 = Vector3(1, 0, 0)
+
+func _process(delta: float) -> void:
+	if not "--xrsim-automated-tests" in OS.get_cmdline_user_args():
+		# When we're running tests via the XR Simulator, we don't want this
+		# to be animated, which can lead to differences in the screenshots.
+		var displacement = sin(Time.get_ticks_msec() / 300.0)
+		var origin := Vector3(displacement, displacement, displacement)
+		origin = origin * direction
+		var new_transform := Transform3D(basis, origin)
+		multimesh.set_instance_transform(0, new_transform)
