e1e58aca0dba7fef058f3847de61ae5944863e8b
David Snopek <dsnopek@gmail.com>
2025-02-26 17:53:32 -0600

    Passthrough extensions should override real alpha blend mode, if enabled

--- plugin/src/main/cpp/extensions/openxr_htc_passthrough_extension_wrapper.cpp
+++ plugin/src/main/cpp/extensions/openxr_htc_passthrough_extension_wrapper.cpp
@@ -93,8 +93,10 @@ void OpenXRHtcPassthroughExtensionWrapper::_on_instance_destroyed() {
 
 void OpenXRHtcPassthroughExtensionWrapper::_on_session_created(uint64_t p_session) {
 	if (htc_passthrough_ext) {
-		// If it's already supported, then we don't want to emulate it.
-		if (get_openxr_api()->is_environment_blend_mode_alpha_supported() != OpenXRAPIExtension::OPENXR_ALPHA_BLEND_MODE_SUPPORT_NONE) {
+		// If another extension is already emulating alpha blend mode, then we don't attempt to.
+		if (get_openxr_api()->is_environment_blend_mode_alpha_supported() == OpenXRAPIExtension::OPENXR_ALPHA_BLEND_MODE_SUPPORT_EMULATING) {
+			// Act as if the extension is not enabled.
+			htc_passthrough_ext = false;
 			return;
 		}
 
