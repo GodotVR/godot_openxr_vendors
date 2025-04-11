484d14b1c8ea4c210002a7c601ed640ddf48f492
devloglogan <devloglogan@gmail.com>
2025-04-11 08:18:24 -0500

    Improve hand tracking related code in demo project

---
+++ demo/hand_mesh.gd
@@ -0,0 +1,8 @@
+extends XRNode3D
+
+func _process(_delta: float) -> void:
+	var hand_tracker: XRHandTracker = XRServer.get_tracker(tracker)
+	if hand_tracker and hand_tracker.has_tracking_data:
+		show()
+	else:
+		hide()
