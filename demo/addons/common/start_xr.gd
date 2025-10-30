0ca9ae27065e77f8a63396e17baeb2df50aab96e
dhoverb <dhover@google.com>
2025-10-30 18:32:08 -0500

    Add addons/common (#7)

--- demo/addons/common/start_xr.gd
+++ demo/addons/common/start_xr.gd
@@ -8,7 +8,7 @@ signal focus_lost
 signal focus_gained
 signal pose_recentered
 
-@export var maximum_refresh_rate: int = 90
+@export var maximum_refresh_rate := 90
 
 var xr_interface: OpenXRInterface
 var xr_is_focussed = false
