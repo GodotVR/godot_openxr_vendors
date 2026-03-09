5bbe87bc536b1938b4d7a63d362b3f5f32f05e9d
dhoverb <dhover@google.com>
2026-03-09 17:50:39 +0000

    Implement XR_ANDROID_raycast

--- plugin/src/main/cpp/register_types.cpp
+++ plugin/src/main/cpp/register_types.cpp
@@ -55,6 +55,7 @@
 #include "extensions/openxr_android_light_estimation_extension.h"
 #include "extensions/openxr_android_passthrough_camera_state_extension.h"
 #include "extensions/openxr_android_performance_metrics_extension.h"
+#include "extensions/openxr_android_raycast_extension.h"
 #include "extensions/openxr_android_recommended_resolution_extension.h"
 #include "extensions/openxr_android_scene_meshing_extension.h"
 #include "extensions/openxr_android_trackables_extension.h"
@@ -99,6 +100,7 @@
 
 #include "classes/openxr_android_anchor_tracker.h"
 #include "classes/openxr_android_environment_depth.h"
+#include "classes/openxr_android_hit_result.h"
 #include "classes/openxr_android_light_estimation.h"
 #include "classes/openxr_android_scene_submesh_data.h"
 #include "classes/openxr_android_trackable_object_tracker.h"
@@ -186,6 +188,8 @@ void initialize_plugin_module(ModuleInitializationLevel p_level) {
 			GDREGISTER_CLASS(OpenXRAndroidSceneSubmeshData);
 			GDREGISTER_CLASS(OpenXRAndroidUnboundedReferenceSpaceExtension);
 
+			GDREGISTER_CLASS(OpenXRAndroidHitResult);
+			GDREGISTER_CLASS(OpenXRAndroidRaycastExtension);
 			GDREGISTER_CLASS(OpenXRAndroidTrackablesExtension);
 			GDREGISTER_CLASS(OpenXRAndroidTrackablesObjectExtension);
 
@@ -391,6 +395,10 @@ void initialize_plugin_module(ModuleInitializationLevel p_level) {
 			if (_get_bool_project_setting("xr/openxr/extensions/androidxr/trackables_object")) {
 				_register_extension_with_openxr(OpenXRAndroidTrackablesObjectExtension::get_singleton());
 			}
+
+			if (_get_bool_project_setting("xr/openxr/extensions/androidxr/raycast")) {
+				_register_extension_with_openxr(OpenXRAndroidRaycastExtension::get_singleton());
+			}
 		} break;
 
 		case MODULE_INITIALIZATION_LEVEL_SERVERS:
@@ -426,6 +434,7 @@ void initialize_plugin_module(ModuleInitializationLevel p_level) {
 			_register_extension_as_singleton(OpenXRAndroidEnvironmentDepthExtension::get_singleton());
 			_register_extension_as_singleton(OpenXRAndroidUnboundedReferenceSpaceExtension::get_singleton());
 			_register_extension_as_singleton(OpenXRAndroidTrackablesExtension::get_singleton());
+			_register_extension_as_singleton(OpenXRAndroidRaycastExtension::get_singleton());
 
 // @todo GH Issue 304: Remove check for meta headers when feature becomes part of OpenXR spec.
 #ifdef META_HEADERS_ENABLED
@@ -613,6 +622,7 @@ void add_plugin_project_settings() {
 	_add_bool_project_setting(project_settings, "xr/openxr/extensions/androidxr/unbounded_reference_space/enable_on_startup", false);
 	_add_bool_project_setting(project_settings, "xr/openxr/extensions/androidxr/trackables", false);
 	_add_bool_project_setting(project_settings, "xr/openxr/extensions/androidxr/trackables_object", false);
+	_add_bool_project_setting(project_settings, "xr/openxr/extensions/androidxr/raycast", false);
 
 // @todo GH Issue 304: Remove check for meta headers when feature becomes part of OpenXR spec.
 #ifdef META_HEADERS_ENABLED
