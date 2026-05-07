afc3394b63db40ededdf828e5315ffc6604bb5ee
dhoverb <dhover@google.com>
2026-05-07 17:28:17 +0000

    Implement XR_ANDROID_trackables_object

---
+++ samples/androidxr-trackables-sample/objects/objects.gd
@@ -0,0 +1,40 @@
+extends Node3D
+
+const OBJECT_TRACKABLE_VISUAL = preload("res://objects/object_trackable_visual/object_trackable_visual.tscn")
+
+var _visuals_dict: Dictionary
+
+
+func _ready():
+	XRServer.tracker_added.connect(_on_tracker_added)
+	XRServer.tracker_removed.connect(_on_tracker_removed)
+
+
+func _on_tracker_added(tracker_name: StringName, _type: int):
+	var tracker: XRTracker = XRServer.get_tracker(tracker_name)
+
+	if not tracker:
+		push_error("Couldn't retrieve tracker!")
+		return
+
+	var trackable_visual: XRAnchor3D
+	if tracker is OpenXRAndroidTrackableObjectTracker:
+		trackable_visual = OBJECT_TRACKABLE_VISUAL.instantiate()
+		trackable_visual.set_object_tracker(tracker)
+	else:
+		return
+
+	add_child(trackable_visual)
+	_visuals_dict[tracker_name] = trackable_visual
+
+
+func _on_tracker_removed(tracker_name: StringName, _type: int):
+	_on_tracker_removed_impl(tracker_name)
+
+
+func _on_tracker_removed_impl(tracker_name: StringName):
+	if !_visuals_dict.has(tracker_name):
+		return
+
+	_visuals_dict[tracker_name].queue_free()
+	_visuals_dict.erase(tracker_name)
