b68ce56a6ee9d68b9198adbb54a4f121869ab6eb
dhoverb <dhover@google.com>
2026-05-15 06:59:56 -0500

    Implement XR_ANDROID_raycast (#456)

--- samples/androidxr-trackables-sample/main.gd
+++ samples/androidxr-trackables-sample/main.gd
@@ -1,7 +1,37 @@
 extends StartXR
 
+const TRANSFORM3D_UTILS = preload("res://transform3d_utils.gd")
+
 
 func _ready():
 	super._ready()
 
 	OS.request_permissions()
+
+	XRServer.get_tracker("head").pose_changed.connect(_wait_for_camera)
+
+
+func _wait_for_camera(pose: XRPose):
+	if pose.name != &"default":
+		return
+
+	if pose.transform == Transform3D.IDENTITY:
+		# still waiting
+		return
+
+	# stop waiting for the camera
+	XRServer.get_tracker("head").pose_changed.disconnect(_wait_for_camera)
+
+	$XROrigin3D/Raycast.initialize()
+
+
+func _process(_delta: float) -> void:
+	# position the menu in front of the camera, when the camera's UP vector is Vector3.UP
+
+	var camera_transform: Transform3D = $XROrigin3D/XRCamera3D.get_camera_transform()
+
+	# apply an offset to the camera, when the camera is upright
+	var menu_transform: Transform3D = TRANSFORM3D_UTILS.upright_then_offset(camera_transform, Vector3(0.0, -0.7, -2.0))
+
+	# make Menu3D look at the camera
+	$XROrigin3D/Menu3D.global_transform = menu_transform.looking_at(camera_transform.origin, Vector3.UP, true)
---
+++ samples/androidxr-trackables-sample/raycast/raycast.gd
@@ -0,0 +1,69 @@
+extends Node3D
+
+@export var scenes: Array[PackedScene]
+
+const TRANSFORM3D_UTILS = preload("res://transform3d_utils.gd")
+
+
+func initialize():
+	if scenes.is_empty():
+		push_error("No scenes! add some in the inspector")
+		return
+
+	# reposition the hud to the left
+	var camera_transform: Transform3D = get_tree().root.get_camera_3d().get_camera_transform()
+	var xform: Transform3D = TRANSFORM3D_UTILS.upright_then_offset(camera_transform, Vector3(-1.5, 0.0, -0.3))
+
+	# and point it towards the camera
+	$Viewport2Din3D.global_transform = xform.looking_at(camera_transform.origin, Vector3.UP, true)
+
+	_free_scenes()
+
+
+# Callback from "menu3d_group"
+func update_menu(menu3d: Menu3D, menu_stack_item: Menu3D.MenuStackItem):
+	if menu_stack_item.current_menu != Menu3D.MENU3D_MAIN:
+		return
+
+	var current_scene_name: String
+	if 0 < $Scene.get_child_count():
+		current_scene_name = $Scene.get_child(0).name
+
+	for scene in scenes:
+		if current_scene_name == scene.get_state().get_node_name(0):
+			menu3d.get_menu2d().add_choice("Stop [%s]" % current_scene_name, _stop_raycasts.bind(menu3d))
+		else:
+			menu3d.get_menu2d().add_choice("Switch raycast mode to [%s]" % scene.get_state().get_node_name(0), _show_scene.bind(menu3d, scene))
+
+
+func _show_scene(menu3d: Menu3D, packed_scene: PackedScene):
+	var scene: Node3D = packed_scene.instantiate()
+	$Viewport2Din3D.get_scene_root().text = scene.hud_instructions
+
+	# free the old scene(s)
+	_free_scenes()
+
+	# and add our new scene
+	$Scene.add_child(scene)
+	$Viewport2Din3D.visible = true
+
+	menu3d.refresh()
+
+
+func _stop_raycasts(menu3d: Menu3D):
+	_free_scenes()
+	menu3d.refresh()
+
+
+func _free_scenes():
+	var children: Array[Node] = $Scene.get_children()
+
+	# there should be only one child, but free all of them in case we somehow have more
+	if 1 < children.size():
+		push_error("Somehow have more than one active scene; freeing all of them")
+
+	for child in children:
+		$Scene.remove_child(child)
+		child.queue_free()
+
+	$Viewport2Din3D.visible = false
